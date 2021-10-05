96a23bc9f9234b09a9fc6c176dd307a0c6bf77c2
ShiniiPanda <celisiagaming@gmail.com>
2021-10-05 21:13:10 +0200

    Add cors functionality as middle-ware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
+const cors = require('cors');
 
 // Start up an instance of app
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
