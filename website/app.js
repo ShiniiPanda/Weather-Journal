71ecd710169ebafa01e2ace9c39f6032fffe35a3
ShiniiPanda <celisiagaming@gmail.com>
2021-10-05 21:41:31 +0200

    Added metric option and included comments

--- website/app.js
+++ website/app.js
@@ -1,8 +1,9 @@
 /* Global Variables */
 
 // Open Weather API Information
-const baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
-const key = "b3671da29b730a0a7b223abfec595c2b";
+const baseURL = "api.openweathermap.org/data/2.5/weather?zip="; // Base url using zip codes.
+const key = "&appid=b3671da29b730a0a7b223abfec595c2b"; // API Key.
+const unit = "&units=metric"; // Using Metric for this journal.
 
 
 // Create a new date instance dynamically with JS
