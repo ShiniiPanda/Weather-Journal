653516542bc5eb90c6d09fb1dea560d2543f8cc3
ShiniiPanda <celisiagaming@gmail.com>
2021-10-05 21:36:06 +0200

    Added baseURL and API KEY values of OpenWeatherAPI

--- website/app.js
+++ website/app.js
@@ -1,5 +1,10 @@
 /* Global Variables */
 
+// Open Weather API Information
+const baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
+const key = "b3671da29b730a0a7b223abfec595c2b";
+
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
