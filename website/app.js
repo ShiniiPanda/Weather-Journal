1c7845dca16cd05db77c5607a86ed3fbca6c5738
ShiniiPanda <celisiagaming@gmail.com>
2021-10-06 20:42:54 +0200

    chained post request to save temperature,date and user response

--- website/app.js
+++ website/app.js
@@ -4,6 +4,7 @@
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip="; // Base url using zip codes.
 const key = "&appid=b3671da29b730a0a7b223abfec595c2b"; // API Key.
 const unit = "&units=metric"; // Using Metric for this journal.
+const postRoute = '/postWeather';
 
 
 // Create a new date instance dynamically with JS
@@ -12,14 +13,18 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 document.getElementById('generate').addEventListener('click', () => {
     let zipcode = document.getElementById('zip').value;
-    getWeather(baseURL+zipcode+key+unit);
+    let userResponse = document.getElementById('feelings').value;
+    getWeather(baseURL+zipcode+key+unit).
+    then(function(weatherData) {
+        postWeather(postRoute, {temp: weatherData.main.temp, date: newDate, response: userResponse});
+    });
 })
 
 async function getWeather(url){
     const res = await fetch(url);
     try {
         const weatherData = await res.json();
-        console.log(weatherData);
+        //console.log(weatherData);
         return weatherData;
     } catch (e) {
         console.log("An error has occured: " + e);
@@ -27,18 +32,18 @@ async function getWeather(url){
     }
 }
 
-async function postWeather(url, data) {
+async function postWeather(url, data = {}) {
     const sendResponse = await fetch(url, {
-        method: "POST",
-        credentials: "same-origin",
+        method: 'POST',
+        credentials: 'same-origin',
         headers: {
-            "Content-Type": "application/json"
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     });
     try {
         const newWeather = await sendResponse.json();
-        return newWeather();
+        return newWeather;
     } catch (e) {
         console.log("An error has occured: " + e);
     }
