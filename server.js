8d2fed45d0f39a7303eff1b8335cd7f26f958620
ShiniiPanda <celisiagaming@gmail.com>
2021-10-06 22:36:11 +0200

    Added modules to allow for local storage of json journal entry

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ projectData = {};
 // Require Express to run server and routes
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser'); // not used due to deprecation ,innate express functionalities has been included instead.
+const fs = require('fs');
+const mostRecent = require('./recentWeather.json'); // To read the latest journal entry enter by the user.
+projectData = mostRecent;
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+//Here we are configuring express instead of body-parser as middle-ware.
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -29,14 +33,19 @@ function running() {
 
 // Get Route for projectData
 
-app.get('/getData', (req, res) => {
+app.get('/getWeather', (req, res) => {
     res.send(projectData);
 });
+
+// Post Route for ProjectData
 
 app.post('/postWeather', (req, res) => {
     let fetchedData = req.body;
     projectData.temp = fetchedData.temp;
     projectData.date = fetchedData.date;
-    projectData.response = fetchedData.resposne;
-    console.log(fetchedData);
+    projectData.response = fetchedData.response;
+    fs.writeFile("./recentWeather.json", JSON.stringify(fetchedData), (e) => {  // This function saves the information to recentWeather.json, incase the server was closed and user wished to see their latest entry.
+       if(e) console.log("Couldn't parse latest entry to json file " + e);
+    })
+    //console.log(fetchedData);
 });
