82d440316b37f8f687646b01cfebc4233e420f12
ShiniiPanda <celisiagaming@gmail.com>
2021-10-06 16:40:55 +0200

    Configure get request to OpenWeatherAPI

--- website/app.js
+++ website/app.js
@@ -1,7 +1,7 @@
 /* Global Variables */
 
 // Open Weather API Information
-const baseURL = "api.openweathermap.org/data/2.5/weather?zip="; // Base url using zip codes.
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip="; // Base url using zip codes.
 const key = "&appid=b3671da29b730a0a7b223abfec595c2b"; // API Key.
 const unit = "&units=metric"; // Using Metric for this journal.
 
@@ -11,16 +11,18 @@ let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 document.getElementById('generate').addEventListener('click', () => {
-    getWeather(baseURL+key+unit);
+    let zipcode = document.getElementById('zip').value;
+    getWeather(baseURL+zipcode+key+unit);
 })
 
 async function getWeather(url){
     const res = await fetch(url);
     try {
-        const weatherData = res.json();
+        const weatherData = await res.json();
         console.log(weatherData);
+        return weatherData;
     } catch (e) {
         console.log("An error has occured: " + e);
+        return -1;
     }
 }
-
